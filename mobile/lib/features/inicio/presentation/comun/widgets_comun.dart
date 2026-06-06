0b5b09eb5184b940e7c9707415ab6401d98d8eb1
Sebastian Pinto <sebastian.pinto2301@alumnos.ubiobio.cl>
2026-06-06 16:05:09 -0400

    Agrego widgets comunes del inicio

---
+++ mobile/lib/features/inicio/presentation/comun/widgets_comun.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+import '../../../../core/configuracion/configuracion_api.dart';
+import '../../../../core/tema/colores_ubb.dart';
+import '../../../../core/servicios/excepcion_api.dart';
+import '../../../../shared/modelos/bicicleta_app.dart';
+import '../../../../shared/modelos/bicicletero_app.dart';
+import '../../../../shared/widgets/chip_estado.dart';
+import '../../../../shared/widgets/snackbar_semantico.dart';
+import '../../../acceso/data/solicitud_guardia_api.dart';
+
+part 'bicicletas_widgets.dart';
+part 'bicicletero_widgets.dart';
+part 'central_widgets.dart';
+part 'encabezado_widgets.dart';
+part 'estado_widgets.dart';
+part 'qr_demostracion.dart';
+part 'qr_widgets.dart';
+part 'solicitud_guardia_widgets.dart';
+part 'utiles_comun.dart';
