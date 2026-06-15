fcaef0db0741eac008360da758d80c41ce28a65e
Sebastian Pinto <sebastian.pinto2301@alumnos.ubiobio.cl>
2026-06-15 12:20:01 -0400

    actualizacion: mobile/lib/features/inicio/application/controlador_notificaciones_inicio.dart

--- mobile/lib/features/inicio/application/controlador_notificaciones_inicio.dart
+++ mobile/lib/features/inicio/application/controlador_notificaciones_inicio.dart
@@ -2,22 +2,27 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 
-import '../../notificaciones/data/notificacion_api.dart';
+import '../../notificaciones/data/notificacion_repository.dart';
 import '../../../shared/modelos/notificacion_app.dart';
 
 class ControladorNotificacionesInicio extends ChangeNotifier {
   ControladorNotificacionesInicio({
-    NotificacionApi? notificacionApi,
-    Duration intervalo = const Duration(seconds: 5),
-  })  : _notificacionApi = notificacionApi ?? NotificacionApi(),
+    required NotificacionRepository notificacionRepository,
+    Stream<void>? eventosTiempoReal,
+    Duration intervalo = const Duration(seconds: 30),
+  })  : _notificacionRepository = notificacionRepository,
+        _eventosTiempoReal = eventosTiempoReal,
         _intervalo = intervalo;
 
-  final NotificacionApi _notificacionApi;
+  final NotificacionRepository _notificacionRepository;
+  final Stream<void>? _eventosTiempoReal;
   final Duration _intervalo;
   final Set<String> _notificacionesConocidas = {};
 
   Timer? _temporizador;
+  StreamSubscription<void>? _suscripcionTiempoReal;
   bool _inicializado = false;
+  bool _actualizando = false;
   int _noLeidas = 0;
   NotificacionApp? _nuevaNotificacion;
 
@@ -26,6 +31,9 @@ class ControladorNotificacionesInicio extends ChangeNotifier {
 
   void iniciar() {
     actualizar();
+    _suscripcionTiempoReal = _eventosTiempoReal?.listen(
+      (_) => actualizar(avisarNuevas: true),
+    );
     _temporizador = Timer.periodic(
       _intervalo,
       (_) => actualizar(avisarNuevas: true),
@@ -33,8 +41,13 @@ class ControladorNotificacionesInicio extends ChangeNotifier {
   }
 
   Future<void> actualizar({bool avisarNuevas = false}) async {
+    if (_actualizando) {
+      return;
+    }
+
+    _actualizando = true;
     try {
-      final notificaciones = await _notificacionApi.listar();
+      final notificaciones = await _notificacionRepository.listar();
       final nuevas = notificaciones
           .where(
             (notificacion) =>
@@ -53,7 +66,10 @@ class ControladorNotificacionesInicio extends ChangeNotifier {
           : null;
       _inicializado = true;
       notifyListeners();
-    } catch (_) {}
+    } catch (_) {
+    } finally {
+      _actualizando = false;
+    }
   }
 
   void marcarNuevaNotificacionMostrada() {
@@ -63,12 +79,13 @@ class ControladorNotificacionesInicio extends ChangeNotifier {
   void marcarTodasLeidas() {
     _noLeidas = 0;
     notifyListeners();
-    _notificacionApi.marcarTodasLeidas().catchError((_) {});
+    _notificacionRepository.marcarTodasLeidas().catchError((_) {});
   }
 
   @override
   void dispose() {
     _temporizador?.cancel();
+    _suscripcionTiempoReal?.cancel();
     super.dispose();
   }
 }
