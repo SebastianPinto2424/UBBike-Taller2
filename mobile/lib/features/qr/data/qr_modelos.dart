5f5db819bcc593fe7a332420ed99df631872c121
Sebastian Pinto <sebastian.pinto2301@alumnos.ubiobio.cl>
2026-06-15 12:20:36 -0400

    agregar: mobile/lib/features/qr/data/qr_modelos.dart

---
+++ mobile/lib/features/qr/data/qr_modelos.dart
@@ -0,0 +1,42 @@
+import '../../../shared/modelos/bicicleta_app.dart';
+import '../../../shared/modelos/bicicletero_app.dart';
+
+class QrTemporalApp {
+  const QrTemporalApp({
+    required this.token,
+    required this.tipo,
+    required this.duracionSegundos,
+    required this.expiraEn,
+    required this.bicicleta,
+    this.bicicletero,
+  });
+
+  final String token;
+  final String tipo;
+  final int duracionSegundos;
+  final DateTime expiraEn;
+  final BicicletaApp bicicleta;
+  final BicicleteroApp? bicicletero;
+
+  factory QrTemporalApp.desdeJson(Map<String, dynamic> json) {
+    final bicicletaJson = json['bicicleta'] as Map<String, dynamic>;
+    final bicicleteroJson = json['bicicletero'] as Map<String, dynamic>?;
+
+    return QrTemporalApp(
+      token: json['token'] as String,
+      tipo: json['tipo'] as String,
+      duracionSegundos: json['duracionSegundos'] as int,
+      expiraEn: DateTime.parse(json['expiraEn'] as String),
+      bicicleta: BicicletaApp(
+        id: bicicletaJson['id'] as String,
+        descripcion: bicicletaJson['descripcion'] as String,
+        fotoUrl: null,
+        activa: true,
+        dentroBicicletero: json['tipo'] == 'RETIRO',
+      ),
+      bicicletero: bicicleteroJson == null
+          ? null
+          : BicicleteroApp.desdeJson(bicicleteroJson),
+    );
+  }
+}
