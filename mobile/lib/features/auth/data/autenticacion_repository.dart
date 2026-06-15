cd9d865cd9480022447ee6fa69ec71bdc307b701
Sebastian Pinto <sebastian.pinto2301@alumnos.ubiobio.cl>
2026-06-15 12:20:33 -0400

    agregar: mobile/lib/features/auth/data/autenticacion_repository.dart

---
+++ mobile/lib/features/auth/data/autenticacion_repository.dart
@@ -0,0 +1,64 @@
+import '../../../shared/modelos/usuario_app.dart';
+import 'autenticacion_api.dart';
+
+class AutenticacionRepository {
+  const AutenticacionRepository(this._api);
+
+  final AutenticacionApi _api;
+
+  Future<ResultadoLogin> iniciarSesion({
+    required String correo,
+    required String contrasena,
+  }) =>
+      _api.iniciarSesion(correo: correo, contrasena: contrasena);
+
+  Future<UsuarioApp> obtenerPerfil() async {
+    final usuario = await _api.obtenerPerfil();
+    return UsuarioApp.fromJson(usuario);
+  }
+
+  Future<ResultadoLogin> refrescarToken({
+    required String usuarioId,
+    required String refreshToken,
+  }) =>
+      _api.refrescarToken(usuarioId: usuarioId, refreshToken: refreshToken);
+
+  Future<void> cerrarSesion({String? refreshToken}) =>
+      _api.cerrarSesion(refreshToken: refreshToken);
+
+  Future<String> registrar({
+    required String nombre,
+    required String rut,
+    required String correo,
+    required String contrasena,
+  }) =>
+      _api.registrar(
+        nombre: nombre,
+        rut: rut,
+        correo: correo,
+        contrasena: contrasena,
+      );
+
+  Future<String> solicitarCambioContrasena(String correo) =>
+      _api.solicitarCambioContrasena(correo);
+
+  Future<ResultadoVerificacionCorreo> verificarCorreo(String token) =>
+      _api.verificarCorreo(token);
+
+  Future<String> completarRegistro({
+    required String token,
+    required String nombre,
+    required String contrasena,
+  }) =>
+      _api.completarRegistro(
+        token: token,
+        nombre: nombre,
+        contrasena: contrasena,
+      );
+
+  Future<String> cambiarContrasena({
+    required String token,
+    required String contrasena,
+  }) =>
+      _api.cambiarContrasena(token: token, contrasena: contrasena);
+}
