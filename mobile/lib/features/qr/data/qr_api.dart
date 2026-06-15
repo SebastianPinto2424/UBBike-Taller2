8a17a1c2464c9cd3d271699c681de61ca00c5f1d
Sebastian Pinto <sebastian.pinto2301@alumnos.ubiobio.cl>
2026-06-15 12:20:11 -0400

    actualizacion: mobile/lib/features/qr/data/qr_api.dart

--- mobile/lib/features/qr/data/qr_api.dart
+++ mobile/lib/features/qr/data/qr_api.dart
@@ -1,50 +1,8 @@
 import '../../../core/servicios/cliente_api.dart';
-import '../../../shared/modelos/bicicleta_app.dart';
-import '../../../shared/modelos/bicicletero_app.dart';
-import '../../../shared/servicios/sesion_actual.dart';
-
-class QrTemporalApp {
-  const QrTemporalApp({
-    required this.token,
-    required this.tipo,
-    required this.duracionSegundos,
-    required this.expiraEn,
-    required this.bicicleta,
-    this.bicicletero,
-  });
-
-  final String token;
-  final String tipo;
-  final int duracionSegundos;
-  final DateTime expiraEn;
-  final BicicletaApp bicicleta;
-  final BicicleteroApp? bicicletero;
-
-  factory QrTemporalApp.desdeJson(Map<String, dynamic> json) {
-    final bicicletaJson = json['bicicleta'] as Map<String, dynamic>;
-    final bicicleteroJson = json['bicicletero'] as Map<String, dynamic>?;
-
-    return QrTemporalApp(
-      token: json['token'] as String,
-      tipo: json['tipo'] as String,
-      duracionSegundos: json['duracionSegundos'] as int,
-      expiraEn: DateTime.parse(json['expiraEn'] as String),
-      bicicleta: BicicletaApp(
-        id: bicicletaJson['id'] as String,
-        descripcion: bicicletaJson['descripcion'] as String,
-        fotoUrl: null,
-        activa: true,
-        dentroBicicletero: json['tipo'] == 'RETIRO',
-      ),
-      bicicletero: bicicleteroJson == null
-          ? null
-          : BicicleteroApp.desdeJson(bicicleteroJson),
-    );
-  }
-}
+import 'qr_modelos.dart';
 
 class QrApi {
-  QrApi() : cliente = ClienteApi(obtenerToken: () => SesionActual.token);
+  const QrApi({required this.cliente});
 
   final ClienteApi cliente;
 
