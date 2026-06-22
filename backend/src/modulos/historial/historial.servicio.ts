80591116d81057c20b73b25b86e308e44d65e567
Sebastian Pinto <sebastian.pinto2301@alumnos.ubiobio.cl>
2026-06-22 04:41:45 -0400

    actualizar backend/src/modulos/historial/historial.servicio.ts

--- backend/src/modulos/historial/historial.servicio.ts
+++ backend/src/modulos/historial/historial.servicio.ts
@@ -87,7 +87,8 @@ const mapearMovimiento = (movimiento: MovimientoCompleto) => ({
   },
   bicicleta: {
     id: movimiento.bicicleta.id,
-    descripcion: movimiento.bicicleta.descripcion
+    descripcion: movimiento.bicicleta.descripcion,
+    numeroSerie: movimiento.bicicleta.numeroSerie
   },
   bicicletero: {
     id: movimiento.bicicletero.id,
