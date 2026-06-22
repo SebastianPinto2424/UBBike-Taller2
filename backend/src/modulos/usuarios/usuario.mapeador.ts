5126e4e9058814242824c14f046f1fb988f63f29
Sebastian Pinto <sebastian.pinto2301@alumnos.ubiobio.cl>
2026-06-22 04:41:49 -0400

    actualizar backend/src/modulos/usuarios/usuario.mapeador.ts

--- backend/src/modulos/usuarios/usuario.mapeador.ts
+++ backend/src/modulos/usuarios/usuario.mapeador.ts
@@ -8,6 +8,7 @@ export const mapearUsuarioPublico = (usuario: Usuario) => ({
   rol: usuario.rol,
   correoVerificado: usuario.correoVerificado,
   registroParcial: usuario.registroParcial,
+  debeCambiarContrasena: usuario.debeCambiarContrasena,
   cuentaActiva: usuario.cuentaActiva,
   eliminadoEn: usuario.eliminadoEn,
   creadoEn: usuario.creadoEn,
