375e65d3d71a420258dd22ff4edea9e0c135eeec
Sebastian Pinto <sebastian.pinto2301@alumnos.ubiobio.cl>
2026-06-22 04:42:13 -0400

    actualizar mobile/lib/features/inicio/presentation/guardia/vista_ingreso_guardia.dart

--- mobile/lib/features/inicio/presentation/guardia/vista_ingreso_guardia.dart
+++ mobile/lib/features/inicio/presentation/guardia/vista_ingreso_guardia.dart
@@ -1,4 +1,8 @@
-part of '../pantalla_principal.dart';
+import 'package:flutter/material.dart';
+
+import 'package:ubbike/shared/widgets/vista_con_tabs.dart';
+import 'package:ubbike/features/inicio/presentation/guardia/vista_escaner_qr_guardia.dart';
+import 'package:ubbike/features/inicio/presentation/guardia/vista_gestion_manual_guardia.dart';
 
 enum ModoIngresoGuardia { qr, manual }
 
@@ -15,22 +19,25 @@ class VistaIngresoGuardia extends StatelessWidget {
   const VistaIngresoGuardia({
     super.key,
     required this.modoInicial,
+    this.onIrAHistorial,
   });
 
   final ModoIngresoGuardia modoInicial;
 
+  final VoidCallback? onIrAHistorial;
+
   @override
   Widget build(BuildContext context) {
-    return _VistaConTabs(
+    return VistaConTabs(
       key: ValueKey(modoInicial),
       initialIndex: modoInicial.indice,
       tabs: [
-        _tabCompacto(Icons.qr_code_scanner, 'QR'),
-        _tabCompacto(Icons.edit_note_outlined, 'Manual'),
+        tabCompacto(Icons.qr_code_scanner, 'QR'),
+        tabCompacto(Icons.edit_note_outlined, 'Manual'),
       ],
-      vistas: const [
-        VistaEscanerQrGuardia(),
-        VistaGestionManualGuardia(),
+      vistas: [
+        VistaEscanerQrGuardia(onMovimientoRegistrado: onIrAHistorial),
+        const VistaGestionManualGuardia(),
       ],
     );
   }
